564585fae1fc01701eab559fb10443fb861edbf4
dankturtle <abrowncownow@gmail.com>
2022-09-22 18:29:39 -0700

    moment js and current functions completed

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,9 @@ var lon;
 var units;
 var exclude;
 var data;
+var current;
+var forecastData;
+var forecast;
 var geocode = [];
 units = "imperial";
 //End Variables/constants
@@ -33,10 +36,73 @@ function getWeather(){
         .then((json)=>{
             console.log(json);
             data = json;
+            displayCurrent();
+            getForecast();
         })
         .catch((error)=>console.log("Error: " + error));
 }
+
+function getForecast(){
+    var forecastURL= "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon +"&appid=" + APIKey + "&units=" + units; 
+    fetch(forecastURL)
+        .then((response)=>response.json())
+        .then((fcast)=>{
+            console.log(fcast);
+            forecastData = fcast;
+            displayForecast();
+        })
+        .catch((error)=>console.log("Error: " + error));
+}
+
+function displayCurrent(){
+current ={
+    desc: data.weather[0].description,
+    temp: data.main.temp,
+    wind: data.wind.speed,
+    humid: data.main.humidity,
+    name: data.name,
+    icon: data.weather[0].icon,
+    date: moment.unix(data.dt).format("dddd MMM, Do YYYY"),
+}
+$("#current-date").text(current.date);
+$("#current-desc").text(current.desc);
+$("#current-temp").text(current.temp);
+$("#current-humidity").text(current.humid);
+$("#city").text(current.name);
+$("#current-icon").attr("src", "http://openweathermap.org/img/wn/" + current.icon + "@2x.png");
+$("#current-weather").show();
+}
+
+function displayForecast(){
+    forecast=[{
+        desc: forecastData.list[0].weather[0].description,
+        temp: forecastData.list[0].main.temp,
+        wind: forecastData.list[0].wind.speed,
+        humid: forecastData.list[0].main.humidity,
+        name: forecastData.list[0].name,
+        icon: forecastData.list[0].weather[0].icon,
+        date: forecastData.list[0].dt,
+    },{
+        desc: forecastData.list[1].weather[0].description,
+        temp: forecastData.list[1].main.temp,
+        wind: forecastData.list[1].wind.speed,
+        humid: forecastData.list[1].main.humidity,
+        name: forecastData.list[1].name,
+        icon: forecastData.list[1].weather[0].icon,
+        date: forecastData.list[1].dt,
+    }
+
+
+    ]
+}
+function init(){
+    $("#current-weather").hide();
+}
 //End Functions Declaration
+
+//Initialize
+init();
+//
 
 //Listeners
 $("#search-btn").click(function(event){
